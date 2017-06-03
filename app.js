848d7b1788cc79c10d850aba2613be7b813e6ef3
Ryno Coetzee <Rynoster@dians-ipad.midstreamcollege.local>
2017-06-03 22:34:03 +0200

    RC:Link Why Attend

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var register = require('./routes/register');
+var whyattend = require('./routes/whyattend');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public'))); //Servers static conten
 app.use('/', index);
 app.use('/users', users);
 app.use('/register', register);
+app.use('/whyattend', whyattend);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/whyattend.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var app = express();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router
+  .get('/', (req,res) => {
+
+    res.render('skeleton', {
+      partials: {
+        header: "header", 
+        content: "whyattend",
+        footer: "footer", 
+        jscript: "jscript"
+        }
+      
+    });
+
+  });
+
+module.exports = router;
