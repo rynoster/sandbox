96af93e705f933fd802d1713904d01f5a7bcb40d
Ryno Coetzee <Rynoster@RynoMac.local>
2017-06-07 20:29:46 +0200

    AWS DB change and Registration page structure

--- db.js
+++ db.js
@@ -3,9 +3,10 @@ var knex = require("knex");
 const db = knex({
     client: "mysql",
     connection: {
-        host: "127.0.0.1",
-        user: "root",
-        database: "test",
+        host: "chirpdb.cmin0wgks0mn.us-east-2.rds.amazonaws.com",
+        user: "sa",
+        password: "ericR380",
+        database: "chirp_dcx2017",
     }
 })
 
