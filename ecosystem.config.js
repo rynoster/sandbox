7e76acf58229f6167960bf3a800788cd75e57f88
Ryno Coetzee <Rynoster@RynoMac.local>
2017-06-09 21:29:04 +0200

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: 'chirpee.io',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+        user: 'ubuntu',
+        host: 'ec2-13-58-23-169.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/ec2chirpee.pem',
+        ref: 'origin/master',
+        repo: 'https://github.com/rynoster/sandbox',
+        path: '/home/ubuntu/server/chirpee',
+        env: {
+            "NODE_ENV": "dev"
+        },
+        env_prod : {
+            "NODE_ENV": "prod"
+        },
+        
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --name chirpee.io --env prod'
+
+        }
+  }
+}
