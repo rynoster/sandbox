5cd1ed1e03358cb8f82a9d3be9dab1b1dc419636
Ryno Coetzee <ryno.coetzee@mgmail.com>
2017-07-15 17:31:11 +0200

    Lint fixes

--- passport.js
+++ passport.js
@@ -1,24 +1,24 @@
 const bcrypt = require("bcrypt-nodejs");
-const knex = require("knex");
+// const knex = require("knex");
 const LocalStrategy = require("passport-local").Strategy;
 const passport = require("passport");
 
 const db = require("./db");
 
 passport.use(new LocalStrategy(authenticate));
-passport.use("local-register", new LocalStrategy({passReqToCallback: true}, register));
+passport.use("local-register", new LocalStrategy({ passReqToCallback: true }, register));
 
-function authenticate(email, password, done){
-    
+function authenticate(email, password, done) {
+
     db("users")
         .where("email", email)
         .first()
         .then((user) => {
-            if (!user || !bcrypt.compareSync(password, user.password) ) {
-                return done(null, false, {message: "Invalid user and password combination"});
+            if (!user || !bcrypt.compareSync(password, user.password)) {
+                return done(null, false, { message: "Invalid user and password combination" });
             }
             done(null, user);
-        }, done)
+        }, done);
 }
 
 function register(req, email, password, done) {
@@ -27,10 +27,10 @@ function register(req, email, password, done) {
         .first()
         .then((user) => {
             if (user) {
-                return done(null, false, {message: "A user with that email address has already been registered."})
+                return done(null, false, { message: "A user with that email address has already been registered." });
             }
             if (password !== req.body.password2) {
-                return done(null, false, {message: "Passwords did not match."})
+                return done(null, false, { message: "Passwords did not match." });
             }
 
             const newUser = {
@@ -38,27 +38,27 @@ function register(req, email, password, done) {
                 last_name: req.body.last_name,
                 email: email,
                 password: bcrypt.hashSync(password)
-            }
+            };
 
             db("users")
                 .insert(newUser)
-                .then ((ids) => {
-                    newUser.id = ids[0]
-                    done(null, newUser)
-                }                )
+                .then((ids) => {
+                    newUser.id = ids[0];
+                    done(null, newUser);
+                });
 
-        })
+        });
 }
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id)
-})
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser((id, done) => {
     db("users")
         .where("id", id)
         .first()
         .then((user) => {
-            done(null, user)
-        }, done)
-})
+            done(null, user);
+        }, done);
+});
