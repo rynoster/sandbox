5ee1c4588a617a51d7abd0253d095f74b593a89f
Ryno Coetzee <ryno.coetzee@mgmail.com>
2017-07-20 19:26:51 +0200

    Mailing capabilities

--- user.js
+++ user.js
@@ -1,7 +1,96 @@
 // const moment = require("moment");
 const _ = require("lodash");
+const hjs = require("hjs");
+const fs = require("fs");
+const bcrypt = require("bcrypt-nodejs");
+const url = require("url");
 
 const db = require("./db");
+const Mail = require("./email");
+
+
+//helper functions
+function getFullUrl(req) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get("host"),
+    // pathname: req.originalUrl
+  });
+}
+
+function randomString(length, chars) {
+
+  let result = "";
+
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+
+  return result;
+
+}
+
+function buildHtmlBody(user, password, fullUrl) {
+
+  const htmlFile = fs.readFileSync(__dirname + "/views/emailAgenda.hjs", "utf8");
+
+  const htmlCompile = hjs.compile(htmlFile);
+  const htmlRender = htmlCompile.render({
+    delegateName: user.first_name + " " + user.last_name,
+    passwordUrl: fullUrl + "/login?email=" + user.email + "&firstLogin=" + password
+    // verifyUrl: fullUrl + "/verify/" + params.token,
+  });
+
+//   console.log(htmlRender);
+
+  return (htmlRender);
+
+}
+
+function mailDelegate(user, req) {
+
+    const genPassword = randomString(10, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    const hashPassword = bcrypt.hashSync(genPassword);
+
+    db("users")
+    .where("id", user.id)
+    .update("password", hashPassword)
+    .update("passwordSent", 1)
+    .then(function(result) {
+        console.log("Password generated and record updated for: " + user.email);
+
+        const mail = new Mail({
+            from: "noreply@chirpee.io",
+            to: "ryno@coetzee.za.com",
+            // to: user.email,
+            subject: "Datacentrix Showcase 2017 - Tailor your agenda",
+            html: buildHtmlBody(user, genPassword, getFullUrl(req)),
+
+            // html: "the <strong>agenda tailor</strong> email for " + user.email,
+            successCallback: function (success) {
+                console.log("Mail sent to " + user.email);
+            },
+            errorCallback: function (err) {
+                console.error("Mail could not be sent to " + user.email);
+            }
+        });
+
+        mail.send();
+
+    })
+    .catch(function() {
+        console.log("Could not generate password or update record for: " + user.email);
+    });
+
+}
+
+function mailDelegates(users, req) {
+
+    _.forEach(users, (user) => {
+
+        mailDelegate(user, req);
+
+    });
+}
+
 
 function User(id) {
     this.id = id;
@@ -58,6 +147,24 @@ User.prototype.updateMySessions = function (id, data, callback) {
         })
         .catch((err) => {
             callback(400);
+        });
+    
+};
+
+User.prototype.delegatePasswordsAll = function (callback, req) {
+
+    db("users")
+        .where("passwordSent", 0)
+        .where("admin", 0)
+        .where("password", null)
+        .where("passwordSent", 0)
+        .limit(1)
+        .then((result) => {
+            mailDelegates(result, req);
+            callback(result); //Callback to delegatePasswords API
+        })
+        .catch((err) => {
+            callback(err);
         });
     
 };
